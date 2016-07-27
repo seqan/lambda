86405087a634e2f5d9307b02b84424cd63ec3298
Hannes Hauswedell <h2+github.com@fsfe.org>
2016-07-27 19:16:28 +0200

    [LEGAL] update license information in the src

--- src/output.hpp
+++ src/output.hpp
@@ -1,25 +1,20 @@
 // ==========================================================================
 //                                  lambda
 // ==========================================================================
-// Copyright (c) 2013-2015, Hannes Hauswedell, FU Berlin
+// Copyright (c) 2013-2016, Hannes Hauswedell <h2 @ fsfe.org>
+// Copyright (c) 2016, Knut Reinert and Freie Universität Berlin
 // All rights reserved.
 //
 // This file is part of Lambda.
 //
-// Lambda is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Lambda is Free Software: you can redistribute it and/or modify it
+// under the terms found in the LICENSE[.md|.rst] file distributed
+// together with this file.
 //
 // Lambda is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
-// You should have received a copy of the GNU General Public License
-// along with Lambda.  If not, see <http://www.gnu.org/licenses/>.
-// ==========================================================================
-// Author: Hannes Hauswedell <hannes.hauswedell @ fu-berlin.de>
 // ==========================================================================
 // output.hpp: contains routines for file-writing
 // ==========================================================================
