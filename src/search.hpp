0a71865ce871db0915700d71f4a900fbc882d0b9
Hannes Hauswedell <h2+github.com@fsfe.org>
2020-01-28 17:17:11 +0100

    [MISC] create multiple translation units [step2]

---
+++ src/search.hpp
@@ -0,0 +1,24 @@
+// ==========================================================================
+//                                  lambda
+// ==========================================================================
+// Copyright (c) 2013-2019, Hannes Hauswedell <h2 @ fsfe.org>
+// Copyright (c) 2016-2019, Knut Reinert and Freie Universität Berlin
+// All rights reserved.
+//
+// This file is part of Lambda.
+//
+// Lambda is Free Software: you can redistribute it and/or modify it
+// under the terms found in the LICENSE[.md|.rst] file distributed
+// together with this file.
+//
+// Lambda is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// ==========================================================================
+// lambda.cpp: Main File for Lambda
+// ==========================================================================
+
+#pragma once
+
+int searchMain(int const argc, char const ** argv);
